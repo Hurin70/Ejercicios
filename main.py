b091349217e23b407946027fc811d0f8aec11580
Dani <134628127+Hurin70@users.noreply.github.com>
2023-06-08 20:55:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ print ('el resultado es '+ str (resultado))'''
 '''EJERCICIO 4'''
 
 
-USER = 'Dani' # User correcto
+'''USER = 'Dani' # User correcto
 PASS = '123' # Pass correcto
 cont = 0
 login = False
@@ -98,6 +98,71 @@ while cont < 3 and login == False:
         print('usuario o contraseña incorrecta')
         cont = cont+1
     if cont == 3:
-        print('usuario bloqueado')
-   
+        print('usuario bloqueado')'''
 
+
+'''_____________________________________________________________________________________________'''
+
+'''Frameworks de empleo: "Data Science"
+    -Spring
+    -Muc
+    -Asp.net
+    -Core MVC
+    -Django (desarrollo app web)
+
+__________________________________________________________________________________________________
+
+Al importa funciones para python
+
+Clase:
+    Propiedades: 
+        Metodos == Funciones
+Ejemplo:
+
+from math import floor, ceil
+
+        from = ruta de clar
+        math = clase de artchivo a importar
+        import = acción que pedimos
+        Floor and ceil, son las funciones que importamos
+        
+___________________________________________________________________________________________________
+
+  input % + (un numero)
+   Esto, divide el numero que se mete por imput por el numero que se indique, y te muestra el resto
+___________________________________________________________________________________________________
+
+Podemos utilizar el SubString de otros lenguajes de programación
+EJEMPLOS:'''
+
+'''s = 'Hello, everybody!'
+>>> s[0] #(Aquí, se le puede inidcar un valor, si se le indica cero o nada, empieza desde el principio y solo coge la letra inical)
+'H'
+>>> s[:3] #(cuando no se le indica el valor inicial, inicia desde cero hasta la posición 3)
+'Hel'
+>>> s[2:5] #(aquí se indica que empiece desde la posición 2 hasta la posición 5) 
+'llo'
+'''
+'''
+______________________________________________________________________________________________________
+CONGETURA DE COLLATZ
+    
+num = int(input('Introduzca un numero '))
+
+while num != 1: #si el numero NO es 1 se mantiene el bucle
+    if (num % 2 == 0):# si sigue en el bucle (el numero se divide entre 2 y te da el resto) "num%2", si el resto es 0, el numero es par
+        num = num /2 # sise cumple el "if" y el numero es par, se divide entre 2
+    else:
+        num = (num * 3) + 1  # si no se cumple el if, se entiende que el numero no es par, entonces se multiplica por 3
+    print(num) # cuando se han cumplico las operaciones, print num, muestra todos los pasos por los que ha pasado "num"
+    '''
+'''
+t(f'el v
+
+#Rango
+for i in range(0,10): # se especifica que el rango de valores que se muestran va del 0 al 10-1)
+    prinalor de i es :{i}')
+#Incrementos
+for i in range(0,10,2): # al añadir el 2 en el rango de busqueda, se especifica que se busquen solo los pares)
+    print(f'el valor de i es :{i}')
+'''
